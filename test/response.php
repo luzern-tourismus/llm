d27d12d44dc32863e10bb7659165ab939ba03089
Urs Lang <urs.lang@nemundo.ch>
2026-04-03 20:30:42 +0200

    ,

--- test/response.php
+++ test/response.php
@@ -2,11 +2,15 @@
 
 require __DIR__ . '/../config.php';
 
+
+
+
+
 $request = new \LuzernTourismus\Llm\Core\RequestLlm();
 $request->endpoint = 'https://api.openai.com/v1/responses';
 $request->model = 'gpt-5.4';
-$request->systemPrompt = 'antworte in grossbuchstaben';
-$request->userPrompt = 'weshalb 42';
+$request->systemPrompt = (new \Nemundo\Core\TextFile\Reader\TextFileReader(__DIR__.'/prompt/system.txt'))->getText();  //'Du gibst die Telefon Nr. zurück';
+$request->userPrompt = (new \Nemundo\Core\TextFile\Reader\TextFileReader(__DIR__.'/prompt/user.txt'))->getText();  // 'weshalb 42';
 
 //(new \Nemundo\Core\Debug\Debug())->write($request->userPrompt);
 
